7262c6f7dafe7e323a396ce5dfc371cfa5eeedc6
Z227947 <Z227947@IBMT430PBWCYLK.caremarkrx.net>
2017-11-01 18:28:05 -0400

    edited comments

--- PatternCounter/src/main/java/com/tomtom/patterncounter/main/PatternCounterRunner.java
+++ PatternCounter/src/main/java/com/tomtom/patterncounter/main/PatternCounterRunner.java
@@ -16,11 +16,11 @@ import com.tomtom.patterncounter.service.impl.PatternCounterServiceImpl;
  * 
  * Assumptions used for running the code:
  * 1. Java Version needed is 1.8
- * 2. Input file contents are expected to be in a single line rather than in multi lines
- * 3. Only whole numbers is considered as numbers not decimals
- * 4. If the file does not have minimum 3 words, none of the scenarios are executed and returns empty
- * 5. For Scenario 3 (Phrases) if there are not enough words for the last phrase, then that line is skipped
- * 6. Cucumber is used as tool for BDD and build tool is Maven
+ * 2. Input file contents are expected to be in a single line rather than multi lines
+ * 3. Only whole numbers are considered as numbers not decimals
+ * 4. If the file does not have minimum 3 words, none of the scenarios gets executed and returns empty
+ * 5. For Scenario 3 (Phrases) if there are not enough words to make 3 consecutive words for the last phrase, then that line is skipped
+ * 6. Cucumber is used as tool for BDD and Maven as build tool
  * 7. Run time exception with proper message is thrown if there occurs any exception scenarios
  * 8. IO and core logic are loosely coupled. IO wired via Dependency Injection and Service class uses Factory
  ***/
